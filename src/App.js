f2a107af05a036eebd6176f2fdefc272ba0d3d4d
David Borah <dborah87@gmail.com>
2018-07-27 19:09:43 -0400

    Minor CSS changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        This is going to be the weather app front end.
+      <div className="App-container">
+        <div className="Inner">
+        <img className="Icon" src="http://icons.iconarchive.com/icons/graphicloads/100-flat/256/rain-icon.png" />
+        <div className="Lat-Lon">
+        <input className="input" placeholder="LAT" />
+        <input className="input" placeholder="LAT" />
+        </div>
+        <button className="button">GO</button>
+        </div>
       </div>
     );
   }
