044261e16d6e44c767656f775bfa9fe632533fdd
David Borah <dborah87@gmail.com>
2018-07-27 18:49:59 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        This is going to be the weather app front end.
+      </div>
+    );
+  }
+}
+
+export default App;
