fbd1df4f7846d1cafa5cf6d97afc3c7ad703dc7f
David Borah <dborah87@gmail.com>
2018-08-01 19:26:28 -0400

    attempt to fix gh-pages deployment

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
         <App />
     </Router>,
 document.getElementById('root'));
